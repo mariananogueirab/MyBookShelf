fc4758970e2285a69cfd56225e0d2fc9eb9ee127
mariananogueirab <mariananbp@gmail.com>
2022-10-03 21:58:36 -0300

    check if book already exist

--- src/main/java/com/myBookShelf/service/LibraryService.java
+++ src/main/java/com/myBookShelf/service/LibraryService.java
@@ -1,11 +1,28 @@
 package com.myBookShelf.service;
 
+import com.myBookShelf.controller.Book;
+import com.myBookShelf.repository.LibraryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class LibraryService {
+    @Autowired
+    LibraryRepository repository;
 
-    public String buildId (String isbn, String year) {
+    public String buildId (String isbn, int year) {
         return isbn + "-" + year;
+    }
+
+    public boolean checkIfBookAlreadyExists(String id) {
+        Optional<Book> book = repository.findById(id);
+        if(book.isPresent()) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
