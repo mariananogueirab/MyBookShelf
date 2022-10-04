5154a005b183aa3c48c3a3fb96e9e8ff1d2b38c6
mariananogueirab <mariananbp@gmail.com>
2022-10-03 21:05:51 -0300

    add ComponentScan

--- src/main/java/com/myBookShelf/MyBookShelfApplication.java
+++ src/main/java/com/myBookShelf/MyBookShelfApplication.java
@@ -2,8 +2,10 @@ package com.myBookShelf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.myBookShelf.controller", "com.myBookShelf.repository", "com.myBookShelf.service"})
 public class MyBookShelfApplication {
 
 	public static void main(String[] args) {
