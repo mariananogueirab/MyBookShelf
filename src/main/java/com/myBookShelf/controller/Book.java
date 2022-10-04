20d220dd0f7ac91ea1f5897975a71ad18eff6d93
mariananogueirab <mariananbp@gmail.com>
2022-10-03 21:39:50 -0300

    Book entity

---
+++ src/main/java/com/myBookShelf/controller/Book.java
@@ -0,0 +1,38 @@
+package com.myBookShelf.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name="library")
+public class Book {
+    @Column(name="id")
+    @Id
+    private String id;
+
+    @Column(name = "bookName")
+    private String bookName;
+
+    @Column(name = "isbn")
+    private String isbn;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "imgUrl")
+    private String imgUrl;
+
+    @Column(name = "year")
+    private int year;
+
+    @Column(name = "isRead")
+    private boolean isRead;
+
+}
