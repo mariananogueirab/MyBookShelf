bd0f7c51a5ba250379b569a2bb9123dd5fe7ef84
mariananogueirab <mariananbp@gmail.com>
2022-10-03 21:45:37 -0300

    add annotation repositry

--- src/main/java/com/myBookShelf/repository/LibraryRepository.java
+++ src/main/java/com/myBookShelf/repository/LibraryRepository.java
@@ -2,6 +2,8 @@ package com.myBookShelf.repository;
 
 import com.myBookShelf.controller.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LibraryRepository extends JpaRepository<Book, String> {
 }
